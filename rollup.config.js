ba1e29211740c86cbf389755fd5ccb521ba0a172
dwqs <461147874@qq.com>
2017-08-04 22:32:15 +0800

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+    entry: './index.js',
+    format: 'umd',
+    moduleName: 'TinyDom',
+    dest: 'dist/index.js',
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            externalHelpers: false
+        })
+    ]
+}
