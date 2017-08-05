680f07960c68d068e5baa721d32ce0be96029619
dwqs <461147874@qq.com>
2017-08-06 03:00:54 +0800

    update tests files

--- test/dom.js
+++ test/dom.js
@@ -6,13 +6,70 @@ const domString = `
         <head>
             <title>Test</title>
             <style type="text/css">
+                *{
+                    box-sizing: border-box
+                }
+                .p {
+                    margin: 10px;
+                    padding: 10px
+                }
                 .test{
-                    color: red
+                    color: red;
+                    width: 50px;
+                    height: 100px;
+                }
+                .test::before{
+                    position: absolute;
+                    content: 'sdas',
+                    width: 20px;
+                    height: 20px;
+                }
+                .test::after{
+                    position: absolute;
+                    content: 'sdas',
+                    width: 40px;
+                    height: 40px;
+                }
+                .test2{
+                    color: blue;
+                    font-size: 14px
+                }
+                .span{
+                    border: 1px solid red;
+                    display: inline-block;
+                    margin-top: 10px;
+                    margin-bottom: 20px;
+                }
+                .div{
+                    position: relative;
+                    height: 100px;
+                    width: 100px;
+                    background: blue
+                }
+                .abtest{
+                    position: fixed;
+                    height: 200px;
+                    width: 200px;
+                    background: red
                 }
             </style>
         </head>
         <body>
-            <p class="test">test content</p>
+            <h2>this is h2 element.</h2>
+            this is a text node.
+            <p class="test p">test content</p>
+            this is a text node.
+            <span class="span">this is a span element</span>
+            <div class="div">
+                <div>
+                    <p>
+                        <span id="hello"> hello, span</span>
+                    </p>
+                </div>
+            </div>
+            <p>
+                <div class="abtest"></div>
+            </p>
         </body>
     </html>
 `;
--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,195 @@ import test from 'ava';
 import { window, document } from './dom';
 import { css, offset, createTinyDOM } from '../src/index';
 
-test('index', (t) => {
-    console.log('index');
-    t.pass();
+const p = document.querySelector('.test');
+const span = document.querySelector('.span');
+const text = p.nextSibling;
+
+let dom = createTinyDOM(p);
+let s = createTinyDOM(span);
+
+test.beforeEach(t => {
+    p.className = 'test p';
+    span.className = 'span';
+});
+
+test('The parameter of createTinyDOM must be a DOM Element', (t) => {
+    t.plan(7);
+
+    const error = t.throws(() => {
+        createTinyDOM();
+    }, Error);
+
+    const error2 = t.throws(() => {
+        createTinyDOM('test');
+    }, Error);
+
+    const error3 = t.throws(() => {
+        createTinyDOM(text);
+    }, Error);
+
+    t.true(error2.message.includes('DOM Element'));
+    t.true(error instanceof Error);
+    t.true(error3.message.includes('DOM Element'));
+    t.true(dom.el.nodeType === 1);
+});
+
+test('el property of instance', (t) => {
+    t.plan(2);
+
+    t.deepEqual(p, dom.el);
+    t.deepEqual(span, s.el);
+});
+
+test('Return an array of the class names on the element', (t) => {
+    t.plan(1);
+    t.deepEqual([ 'test', 'p' ], dom.toArray());
+});
+
+test('Check whether the element has the given class', (t) => {
+    t.plan(7);
+
+    const error = t.throws(() => {
+        dom.hasClass(2);
+    }, Error);
+    const error2 = t.throws(() => {
+        dom.hasClass('');
+    }, Error);
+
+    t.false(dom.hasClass('ppp'));
+    t.true(dom.hasClass('test'));
+    t.true(dom.hasClass('p'));
+    t.true(error.message.includes('not a string'));
+    t.true(error2.message.includes('not be empty'));
+});
+
+test('Add the given class in the class list if it doesn\'t exist', (t) => {
+    t.plan(8);
+
+    t.true(dom.hasClass('test'));
+    t.false(dom.hasClass('ppp'));
+
+    let v = dom.addClass('test ppp');
+
+    t.true(dom.hasClass('ppp'));
+    t.deepEqual([ 'test', 'p', 'ppp' ], dom.toArray());
+    t.deepEqual(dom, v);
+
+    s.classList = null;
+
+    t.false(s.hasClass('t'));
+    t.true(s.hasClass('span'));
+
+    s.addClass('t t1');
+
+    t.deepEqual([ 'span', 't', 't1' ], s.toArray());
+});
+
+test('Remove the given class in the class list if it\'s already exist.', (t) => {
+    t.plan(10);
+
+    t.true(dom.hasClass('test'));
+    t.false(dom.hasClass('ppp'));
+
+    dom.addClass('ppp');
+
+    t.true(dom.hasClass('ppp'));
+
+    dom.removeClass('test ppp');
+
+    t.false(dom.hasClass('test'));
+    t.false(dom.hasClass('ppp'));
+
+    s.classList = null;
+
+    t.true(s.hasClass('span'));
+
+    s.removeClass('span');
+
+    t.false(s.hasClass('span'));
+
+    let v = s.addClass('span1 span2 span3');
+
+    t.deepEqual([ 'span1', 'span2', 'span3' ], s.toArray());
+
+    s.removeClass(/span/);
+
+    t.deepEqual([], s.toArray());
+    t.deepEqual(s, v);
+});
+
+test('Toggle the given class in the class list. Optionally force state via force', (t) => {
+    t.plan(5);
+
+    t.true(dom.hasClass('test'));
+
+    dom.toggle('test');
+
+    t.false(dom.hasClass('test'));
+
+    s.classList = null;
+
+    t.false(s.hasClass('test'));
+
+    let v = s.toggle('test');
+
+    t.true(s.hasClass('test'));
+    t.deepEqual(s, v);
+});
+
+test('Replaces an existing class with a new class', (t) => {
+    t.plan(7);
+
+    t.true(dom.hasClass('test'));
+    t.false(dom.hasClass('pppp'));
+
+    let v = dom.replaceClass('test', 'pppp');
+
+    t.false(dom.hasClass('test'));
+    t.true(dom.hasClass('pppp'));
+    t.deepEqual(dom, v);
+
+    s.classList = null;
+
+    t.true(s.hasClass('span'));
+
+    s.removeClass('span', 'span');
+
+    t.false(s.hasClass('span'));
+});
+
+test('Attributes\' getter/setter of the element', (t) => {
+    t.plan(3);
+
+    t.is('span', s.el.getAttribute('class'));
+
+    let v = s.attr('class', 't1 t2');
+
+    t.is('t1 t2', s.el.getAttribute('class'));
+    t.deepEqual(s, v);
+});
+
+test('Return the node name of the element', (t) => {
+    t.plan(2);
+
+    t.is('span', s.getNodeName());
+    t.is('p', dom.getNodeName());
+});
+
+test('Return the width of the element', (t) => {
+    t.plan(1);
+
+    t.is('50px', dom.width());
+});
+
+test('Return the height of the element', (t) => {
+    t.plan(1);
+
+    t.is('100px', dom.height());
+});
+
+test('Return the element\'s bounding box', (t) => {
+    t.plan(1);
+
+    t.deepEqual(p.getBoundingClientRect(), dom.rect());
 });
