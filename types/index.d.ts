6dea290d9cbb5266e60fa505fa621fd7d35d73da
dwqs <461147874@qq.com>
2017-08-05 23:23:04 +0800

    update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,9 +5,14 @@ export interface Box {
     height: number
 }
 
+declare class TinyDOM {
+    constructor(node: HTMLElement);
+}
+
 declare namespace tinyDom {
     export function css(node: HTMLElement, properyty: string, pseudoEle: string | null): string;
-    export function offset(node: HTMLElement): Box
+    export function offset(node: HTMLElement): Box;
+    export function createTinyDOM(node: HTMLElement): TinyDOM
 }
 
 export = tinyDom;
