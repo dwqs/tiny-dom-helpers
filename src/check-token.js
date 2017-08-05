8e4fd24bbbd4bdddf72eba3c35d144ef93982c95
dwqs <461147874@qq.com>
2017-08-05 20:41:29 +0800

    update error msg

--- src/check-token.js
+++ src/check-token.js
@@ -2,21 +2,21 @@ export default function checkToken (token, method, isAllowSpace = false) {
     let m = method || 'a method';
     if (typeof token !== 'string') {
         throw new TypeError(
-            'Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
+            '[tiny-dom-helpers]: Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
             'the token provided (\'' + token + '\') is not a string.'
         );
     }
 
     if (token === '') {
         throw new SyntaxError(
-            'Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
+            '[tiny-dom-helpers]: Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
             'the token provided must not be empty.'
         );
     }
 
     if (!isAllowSpace && /\s/.test(token)) {
         throw new Error(
-            'Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
+            '[tiny-dom-helpers]: Failed to execute \'' + method + '\' on \'TinyDOM\': ' +
             'the token provided (\'' + token + '\') contains HTML space ' +
             'characters, which are not valid in tokens.'
         );
