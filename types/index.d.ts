620da4792fae66213d3530e04500572a2f1f6233
dwqs <461147874@qq.com>
2017-08-06 21:46:39 +0800

    update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,11 +2,30 @@ export interface Box {
     top: number,
     left: number,
     width: number,
-    height: number
+    height: number,
+    right?: number,
+    bottom?: number
 }
 
-declare class TinyDOM {
+export declare class TinyDOM {
     constructor(node: HTMLElement);
+    toArray(): string[];
+    hasClass(cls: string): boolean;
+    addClass(cls: string): TinyDOM;
+    removeClass(cls: string): TinyDOM;
+    toggle(cls: string): TinyDOM;
+    replaceClass(oldClass: string, newClass: string): TinyDOM;
+    attr(prop: string, value: string): TinyDOM;
+    getNodeName(): stirng;
+    width(): stirng;
+    height(): stirng;
+    rect(): Box;
+    offsetParent(): HTMLElement;
+    positions(): Box;
+    previous(): HTMLElement;
+    next(): HTMLElement;
+    parent(): HTMLElement;
+    contains(): boolean
 }
 
 declare namespace tinyDom {
